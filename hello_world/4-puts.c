61916c9d546db83343b7d0d134324213bd49d535
kape <kapetillosky8612@outlook.com>
2024-02-19 12:49:13 -0400

    Displaying puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -5,7 +5,7 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{puts(str1);
-puts(\"Programming is like building a multilingual puzzle,followed by a new line.\n);
+{
+	puts(\"Programming is like building a multilingual puzzle);
 return (0);
 }
