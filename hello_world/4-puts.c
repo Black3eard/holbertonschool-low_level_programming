a8efab2e719c42415e4332cb545b586bf90ec4f2
kape <kapetillosky8612@outlook.com>
2024-02-19 12:43:13 -0400

    Displaying puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
-{   puts(str1);
-    puts(\"Programming is like building a multilingual puzzle, followed by a new line.\n);
-
-
-    return (0);
+{puts(str1);
+puts(\"Programming is like building a multilingual puzzle,followed by a new line.\n);
+return (0);
 }
