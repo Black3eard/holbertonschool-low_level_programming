010fa979a263d23ebcf070ee3f9be80e899e6b5b
kape <kapetillosky8612@outlook.com>
2024-02-20 15:41:29 -0400

    The last dig

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 	}
 	if (udig == 0)
 	{
-		printf("Last digit of %d is %d and 0\n", n, udig);
+		printf("Last digit of %d is %d and is 0\n", n, udig);
 	}
 	if (udig != 0 && udig < 6)
 	{
