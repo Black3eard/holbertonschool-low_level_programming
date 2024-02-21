4921ceee4a93f3ca8b55ed74fd85148616f2a24a
kape <kapetillosky8612@outlook.com>
2024-02-21 14:22:55 -0400

    Numbers 0-9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int decima;
+
+	for (decima = 0; decima <= 9; decima++)
+		printf("%d", decima);
+	printf("\n");
+	return (0);
+}
