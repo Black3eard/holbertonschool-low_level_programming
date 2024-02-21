ac522e3beda1b43280eac5147883ed4cc023e87b
kape <kapetillosky8612@outlook.com>
2024-02-21 14:43:21 -0400

    Number but putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -Entry Point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char decima;
+
+	for (decima = 0; decima <= 9; decima++)
+	{
+		putchar(decima);
+	}
+	putchar('\n');
+		return (0);
+}
