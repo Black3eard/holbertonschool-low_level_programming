92c307d061992fb3230c9635d33edafdfd01d190
kape <kapetillosky8612@outlook.com>
2024-02-23 09:18:27 -0400

    is lower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * islower - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+	if (c != 99)
+	{
+		_putchar('0');
+	}
+	if (c == 99)
+	{
+		_putchar('1');
+	}
+	return (0);
+}
