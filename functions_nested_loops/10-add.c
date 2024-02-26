3362bdac30cd648c41d687289bf243bfaffb03e9
Black3eard <kapetillosky8612@outlook.com>
2024-02-25 22:04:30 -0400

    Add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - check the code
+ * @a: an argument.
+ * @b: an argument
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+	int x = a + b;
+
+	return (x);
+}
