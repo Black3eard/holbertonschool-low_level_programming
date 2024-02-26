641bb5b8b037bcb887444a31201b547eb0464468
Black3eard <kapetillosky8612@outlook.com>
2024-02-26 16:20:34 -0400

    Number 0-9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0 ; n <= 9; n++)
+	{
+		_putchar(n + '0');
+	}
+	_putchar('\n');
+}
