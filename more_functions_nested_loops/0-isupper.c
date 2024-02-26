b444c3b9b9bd09cc0e4560895ed16b742950bc03
Black3eard <kapetillosky8612@outlook.com>
2024-02-26 15:52:26 -0400

    Is upper

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,1 +1,13 @@
-#include "main.h
+#include "main.h"
+/**
+ * _isupper - check the code.
+ * @c: an argument for upper
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
