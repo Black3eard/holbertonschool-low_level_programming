d3315b88284982b162f318d18c711cb118871b8b
Black3eard <kapetillosky8612@outlook.com>
2024-02-27 10:59:22 -0400

    Lines lon and shor

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -10,10 +10,10 @@ void print_line(int n)
 
 	if (n <= 0)
 		_putchar('\n');
-		else
-			for (l = 0; l < n; l++)
-			{
-				_putchar('_');
-			}
+	else
+	{
+		for (l = 0; l < n; l++)
+			_putchar('_');
 		_putchar('\n');
+	}
 }
