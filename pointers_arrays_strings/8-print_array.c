7d2948071ff4a29ec6193f5fb7a10a65c227351a
Black3eard <kapetillosky8612@outlook.com>
2024-03-03 11:18:39 -0400

    Print array until n

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - check the code.
+ * @a: an argument.
+ * @n: an argument.
+ * Return: void.
+ */
+void print_array(int *a, int n)
+{
+	int count;
+
+	for (count = 0; a[count] != '\0'; count++)
+	{
+		if (count <= n)
+		{
+			printf("%d, ", a[count]);
+		}
+	}
+	printf("\n");
+}
