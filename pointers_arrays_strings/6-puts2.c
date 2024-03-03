e13c66ef72743f1bae2cf44232d1857da9819c32
Black3eard <kapetillosky8612@outlook.com>
2024-03-03 09:15:56 -0400

    Two by two

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -9,9 +9,12 @@ void puts2(char *str)
 	int count;
 	int disp;
 
-	for (count = 0, disp = 0; str[count] != '\0' && str[disp] != '\0'; count ++, disp += 2)
+	for (count = 0; str[count] != '\0'; count++)
+	{}
+	for (disp = 0; disp < count; disp += 2)
 	{
 		_putchar(str[disp]);
 	}
-		_putchar('\n');
+
+	_putchar('\n');
 }
