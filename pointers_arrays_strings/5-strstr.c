6b914be908ca26e230a4361fe2e559e1c6df06fa
Black3eard <kapetillosky8612@outlook.com>
2024-03-05 19:16:04 -0400

    Substring

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -7,7 +7,7 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-for (; *haystack != '\0'; haystack++)
+	for (; *haystack != '\0'; haystack++)
 	{
 		char *uno = haystack;
 		char *dos = needle;
