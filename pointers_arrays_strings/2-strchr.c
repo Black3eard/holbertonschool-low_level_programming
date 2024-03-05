10cb3a38e074307af0c6800acbb67f0e1485f0cd
Black3eard <kapetillosky8612@outlook.com>
2024-03-05 13:36:25 -0400

    Half of it

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strchr - check the code
+ * @s: an argument
+ * @c: an argument
+ * Return: char.
+ */
+char *_strchr(char *s, char c)
+{
+	int find;
+
+	for (find = 0; s[find] != '\0' && s[find] != c; find++)
+	{}
+	if ( s[find] == c)
+	{
+		return (s + find);
+	}
+	else
+	{
+		return ('\0');
+	}
+}
+
